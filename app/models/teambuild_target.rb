e0f77bc7fc4daae20331bc5b8142245ba3e2ea85
Robin Ward <robin.ward@gmail.com>
2019-11-15 16:36:59 -0500

    Updated DB

--- app/models/teambuild_target.rb
+++ app/models/teambuild_target.rb
@@ -2,4 +2,11 @@
 
 class TeambuildTarget < ActiveRecord::Base
   belongs_to :user
+  before_create :default_position
+
+  # This is not safe under concurrency, but it's unlikely someone will be creating
+  # targets that actively.
+  def default_position
+    self.position = (TeambuildTarget.maximum(:position) || 0) + 1
+  end
 end
--- db/migrate/20191112145430_create_teambuild_targets.rb
+++ db/migrate/20191112145430_create_teambuild_targets.rb
@@ -6,6 +6,7 @@ class CreateTeambuildTargets < ActiveRecord::Migration[5.2]
       t.string :name, null: false
       t.integer :target_type_id, null: false
       t.integer :group_id, null: true
+      t.integer :position, null: false
       t.timestamps null: false
     end
   end
