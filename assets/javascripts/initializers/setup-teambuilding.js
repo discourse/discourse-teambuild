36560454ee08dee6d0523450f3d9f3081b68b9a7
OsamaSayegh <asooomaasoooma90@gmail.com>
2023-12-08 09:20:38 +0300

    FIX: Migrate legacy hamburger menu links to sidebar

--- assets/javascripts/initializers/setup-teambuilding.js
+++ assets/javascripts/initializers/setup-teambuilding.js
@@ -5,14 +5,24 @@ export default {
   initialize() {
     withPluginApi("0.8", (api) => {
       const currentUser = api.getCurrentUser();
-      const teamBuildName = api.container.lookup(
-        "service:site-settings"
-      ).teambuild_name;
-      if (currentUser && currentUser.can_access_teambuild) {
-        api.decorateWidget("hamburger-menu:generalLinks", () => {
-          return {
-            route: "teamBuild.progress",
-            rawLabel: teamBuildName,
+      if (currentUser?.can_access_teambuild) {
+        api.addCommunitySectionLink((baseSectionLink) => {
+          return class TeambuildSectionLink extends baseSectionLink {
+            get name() {
+              return "team-building";
+            }
+
+            get route() {
+              return "teamBuild.progress";
+            }
+
+            get text() {
+              return this.siteSettings.teambuild_name;
+            }
+
+            get title() {
+              return this.siteSettings.teambuild_name;
+            }
           };
         });
       }
