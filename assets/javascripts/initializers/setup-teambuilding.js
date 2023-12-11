49a4959b3b5d3b12de8747abd4b16ed69c527491
Osama Sayegh <asooomaasoooma90@gmail.com>
2023-12-11 12:59:03 +0300

    FIX: Migrate legacy hamburger menu links to sidebar (#66)
    The legacy hamburger menu, which is implemented using the `hamburger-menu` widget, is being removed from core, so we need to migrate all customizations that currently target the legacy hamburger menu to the new sidebar.
    
    This commit migrates the link that the plugin adds to the legacy hamburger menu to the new sidebar.
    
    Internal topic: t/113137.

--- assets/javascripts/initializers/setup-teambuilding.js
+++ assets/javascripts/initializers/setup-teambuilding.js
@@ -5,14 +5,13 @@ export default {
   initialize() {
     withPluginApi("0.8", (api) => {
       const currentUser = api.getCurrentUser();
-      const teamBuildName = api.container.lookup(
-        "service:site-settings"
-      ).teambuild_name;
-      if (currentUser && currentUser.can_access_teambuild) {
-        api.decorateWidget("hamburger-menu:generalLinks", () => {
-          return {
-            route: "teamBuild.progress",
-            rawLabel: teamBuildName,
+      if (currentUser?.can_access_teambuild) {
+        api.addCommunitySectionLink((baseSectionLink) => {
+          return class TeambuildSectionLink extends baseSectionLink {
+            name = "team-building";
+            route = "teamBuild.progress";
+            text = this.siteSettings.teambuild_name;
+            title = this.siteSettings.teambuild_name;
           };
         });
       }
