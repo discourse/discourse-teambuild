5a8b188613b41e5c0064528d2aefa3d6e32853d6
David Taylor <david@taylorhq.com>
2024-02-06 12:05:32 +0000

    DEV: Update adapters to native class syntax (#68)
    In preparation for https://github.com/discourse/discourse/pull/25544

--- assets/javascripts/discourse/adapters/teambuild-progress.js
+++ assets/javascripts/discourse/adapters/teambuild-progress.js
@@ -1,8 +1,8 @@
 import RestAdapter from "discourse/adapters/rest";
 
-export default RestAdapter.extend({
-  jsonMode: true,
+export default class TeambuildProgressAdapter extends RestAdapter {
+  jsonMode = true;
   pathFor(store, type, username) {
     return `/team-build/progress/${username}.json`;
-  },
-});
+  }
+}
