ea7c17972d5106d689f0920907e959bffeca9ac2
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2024-10-25 05:48:55 -0300

    DEV: Update plugin annotations (#82)

--- app/models/teambuild_target_user.rb
+++ app/models/teambuild_target_user.rb
@@ -5,3 +5,19 @@ class TeambuildTargetUser < ActiveRecord::Base
   belongs_to :teambuild_target
   belongs_to :teambuild_target_user, class_name: "User"
 end
+
+# == Schema Information
+#
+# Table name: teambuild_target_users
+#
+#  id                  :bigint           not null, primary key
+#  user_id             :integer          not null
+#  teambuild_target_id :bigint           not null
+#  target_user_id      :integer          not null
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+# Indexes
+#
+#  teambuild_unique_choice  (user_id,teambuild_target_id,target_user_id) UNIQUE
+#
