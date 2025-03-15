e3004f57ce144bf2f640b234bd3d129a5ab3c028
Jarek Radosz <jarek@cvx.dev>
2025-03-15 13:55:54 +0100

    DEV: Update linting

--- assets/javascripts/discourse/adapters/teambuild-target.js
+++ assets/javascripts/discourse/adapters/teambuild-target.js
@@ -2,6 +2,7 @@ import RestAdapter from "discourse/adapters/rest";
 
 export default class TeambuildTargetAdapter extends RestAdapter {
   jsonMode = true;
+
   pathFor(store, type, id) {
     if (id) {
       return `/team-build/targets/${id}.json`;
--- assets/javascripts/discourse/components/teambuild-target.js
+++ assets/javascripts/discourse/components/teambuild-target.js
@@ -1,72 +1,83 @@
 import Component from "@ember/component";
-import { computed } from "@ember/object";
+import { action, computed } from "@ember/object";
 import { equal, or } from "@ember/object/computed";
 import { underscore } from "@ember/string";
+import { tagName } from "@ember-decorators/component";
 import { popupAjaxError } from "discourse/lib/ajax-error";
 import { bufferedProperty } from "discourse/mixins/buffered-content";
 import { Types } from "discourse/plugins/discourse-teambuild/discourse/models/teambuild-target";
 
-export default Component.extend(bufferedProperty("target"), {
-  tagName: "",
-  editSelected: false,
+@tagName("")
+export default class TeambuildTarget extends Component.extend(
+  bufferedProperty("target")
+) {
+  editSelected = false;
 
-  needsGroup: equal("buffered.target_type_id", Types.USER_GROUP),
+  @equal("buffered.target_type_id", Types.USER_GROUP) needsGroup;
+  @or("editSelected", "target.isNew") editing;
 
-  canMoveUp: computed("editing", "index", function () {
+  @computed("editing", "index")
+  get canMoveUp() {
     return !this.editing && this.index > 0;
-  }),
+  }
 
-  canMoveDown: computed("editing", "index", "length", function () {
+  @computed("editing", "index", "length")
+  get canMoveDown() {
     return !this.editing && this.index < this.length - 1;
-  }),
+  }
 
-  editing: or("editSelected", "target.isNew"),
-
-  targetTypes: computed(function () {
+  @computed
+  get targetTypes() {
     return Object.keys(Types).map((key) => {
       return { id: Types[key], name: underscore(key) };
     });
-  }),
+  }
 
-  saveDisabled: computed(
+  @computed(
     "buffered.name",
     "target.isSaving",
     "needsGroup",
-    "buffered.group_id",
-    function () {
-      if (this.target.isSaving) {
-        return true;
-      }
-      let name = this.get("buffered.name");
-      if (!name || name.length === 0) {
-        return true;
-      }
-      if (this.needsGroup && !this.get("buffered.group_id")) {
-        return true;
-      }
-      return false;
+    "buffered.group_id"
+  )
+  get saveDisabled() {
+    if (this.target.isSaving) {
+      return true;
     }
-  ),
 
-  actions: {
-    save() {
-      this.target
-        .save(this.buffered.getProperties("name", "target_type_id", "group_id"))
-        .then(() => {
-          this.set("editSelected", false);
-        })
-        .catch(popupAjaxError);
-    },
-    cancel() {
-      if (this.target.isNew) {
-        return this.removeTarget();
-      } else {
+    let name = this.get("buffered.name");
+    if (!name || name.length === 0) {
+      return true;
+    }
+
+    if (this.needsGroup && !this.get("buffered.group_id")) {
+      return true;
+    }
+
+    return false;
+  }
+
+  @action
+  save() {
+    this.target
+      .save(this.buffered.getProperties("name", "target_type_id", "group_id"))
+      .then(() => {
         this.set("editSelected", false);
-        this.rollbackBuffer();
-      }
-    },
-    destroy() {
-      this.target.destroyRecord().then(() => this.removeTarget());
-    },
-  },
-});
+      })
+      .catch(popupAjaxError);
+  }
+
+  @action
+  cancel() {
+    if (this.target.isNew) {
+      return this.removeTarget();
+    } else {
+      this.set("editSelected", false);
+      this.rollbackBuffer();
+    }
+  }
+
+  @action
+  destroy() {
+    this.target.destroyRecord().then(() => this.removeTarget());
+  }
+}
